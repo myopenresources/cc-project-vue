7f75351d1f44c544002215475d0af76f5efeaf6e
332557712@qq.com <332557712@qq.com>
2021-04-22 16:04:03 +0800

    去除没用的依赖

--- cc-front/src/components/water-marker/WaterMarker.vue
+++ cc-front/src/components/water-marker/WaterMarker.vue
@@ -134,7 +134,7 @@ export default defineComponent({
     };
 
     const waterMarkerDestroy = () => {
-      if (waterMarkerObj !== undefined &&  waterMarkerRef.value) {
+      if (waterMarkerObj !== undefined && waterMarkerRef.value) {
         waterMarkerRef.value.removeChild(waterMarkerObj);
         waterMarkerObj = undefined;
       }
